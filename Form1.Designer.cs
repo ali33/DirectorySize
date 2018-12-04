1099f5d02f22954fe1cf0f13d4dd5fe8d00b6c15
Nguyen Duc Son <sonnguyen.ali@gmail.com>
2018-12-04 10:03:55 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace DirectorySize
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFolderPath = new System.Windows.Forms.TextBox();
+            this.dlgFolderBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnChoseFolder = new System.Windows.Forms.Button();
+            this.btnAnalyzer = new System.Windows.Forms.Button();
+            this.pnlResults = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblStats = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.pnlResults.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtFolderPath
+            // 
+            this.txtFolderPath.Location = new System.Drawing.Point(12, 12);
+            this.txtFolderPath.Name = "txtFolderPath";
+            this.txtFolderPath.Size = new System.Drawing.Size(468, 20);
+            this.txtFolderPath.TabIndex = 0;
+            this.txtFolderPath.Text = "C:\\";
+            // 
+            // btnChoseFolder
+            // 
+            this.btnChoseFolder.Location = new System.Drawing.Point(486, 11);
+            this.btnChoseFolder.Name = "btnChoseFolder";
+            this.btnChoseFolder.Size = new System.Drawing.Size(91, 23);
+            this.btnChoseFolder.TabIndex = 1;
+            this.btnChoseFolder.Text = "Select Folder";
+            this.btnChoseFolder.UseVisualStyleBackColor = true;
+            this.btnChoseFolder.Click += new System.EventHandler(this.btnChoseFolder_Click);
+            // 
+            // btnAnalyzer
+            // 
+            this.btnAnalyzer.Location = new System.Drawing.Point(583, 11);
+            this.btnAnalyzer.Name = "btnAnalyzer";
+            this.btnAnalyzer.Size = new System.Drawing.Size(75, 23);
+            this.btnAnalyzer.TabIndex = 2;
+            this.btnAnalyzer.Text = "Calculate";
+            this.btnAnalyzer.UseVisualStyleBackColor = true;
+            this.btnAnalyzer.Click += new System.EventHandler(this.btnAnalyzer_Click);
+            // 
+            // pnlResults
+            // 
+            this.pnlResults.Controls.Add(this.textBox1);
+            this.pnlResults.Location = new System.Drawing.Point(12, 57);
+            this.pnlResults.Name = "pnlResults";
+            this.pnlResults.Size = new System.Drawing.Size(707, 325);
+            this.pnlResults.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(701, 319);
+            this.textBox1.TabIndex = 0;
+            // 
+            // lblStats
+            // 
+            this.lblStats.Location = new System.Drawing.Point(529, 396);
+            this.lblStats.Name = "lblStats";
+            this.lblStats.Size = new System.Drawing.Size(187, 13);
+            this.lblStats.TabIndex = 4;
+            this.lblStats.Text = "*";
+            this.lblStats.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 396);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(11, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "*";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(665, 12);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(51, 23);
+            this.btnStop.TabIndex = 6;
+            this.btnStop.Text = "Stop!";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 450);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblStats);
+            this.Controls.Add(this.pnlResults);
+            this.Controls.Add(this.btnAnalyzer);
+            this.Controls.Add(this.btnChoseFolder);
+            this.Controls.Add(this.txtFolderPath);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Top Directories";
+            this.pnlResults.ResumeLayout(false);
+            this.pnlResults.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFolderPath;
+        private System.Windows.Forms.FolderBrowserDialog dlgFolderBrowser;
+        private System.Windows.Forms.Button btnChoseFolder;
+        private System.Windows.Forms.Button btnAnalyzer;
+        private System.Windows.Forms.Panel pnlResults;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblStats;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+namespace DirectorySize
+{
+    public partial class Form1 : Form
+    {
+        DirectorySizeCalculator calc = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChoseFolder_Click(object sender, EventArgs e)
+        {
+            var result = dlgFolderBrowser.ShowDialog();
+            if (result == DialogResult.OK || result == DialogResult.Yes)
+            {
+                txtFolderPath.Text = dlgFolderBrowser.SelectedPath;
+
+            }
+        }
+
+        private void btnAnalyzer_Click(object sender, EventArgs e)
+        {
+            calc = new DirectorySizeCalculator(txtFolderPath.Text.Trim(), onSizeUpdated);
+            calc.Calculate();
+        }
+
+        private void onSizeUpdated(SortedDictionary<string, long> sizes, bool isOk)
+        {
+            if (!isOk)
+            {
+                this.Invoke(new MethodInvoker(delegate ()
+                {
+                    btnAnalyzer.Enabled = false;
+                    btnChoseFolder.Enabled = false;
+                    btnStop.Enabled = true;
+                    label1.ForeColor = Color.DarkBlue;
+                    label1.Text = "running...";
+                    List<string> lines = new List<string>();
+                    try
+                    {
+                        foreach (var sz in sizes.OrderByDescending(s => s.Value).Take(100))
+                        {
+                            lines.Add(string.Format("{0}: {1}", sz.Key, GetFileSize(sz.Value)));
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        lines.Clear();
+                    }
+                    if (lines.Count > 0)
+                    {
+                        lblStats.Text = sizes.Count.ToString("#,#") + " directories";
+                        textBox1.Clear();
+                        textBox1.Lines = lines.ToArray();
+                    }
+                }));
+            }
+            else
+            {
+                this.Invoke(new MethodInvoker(delegate ()
+                {
+                    btnAnalyzer.Enabled = true;
+                    btnChoseFolder.Enabled = true;
+                    btnStop.Enabled = false;
+                    label1.ForeColor = Color.Red;
+                    label1.Text = "Finished!";
+                }
+                ));
+            }
+        }
+
+        private string GetFileSize(long byteCount)
+        {
+            string size = "0 Bytes";
+            if (byteCount >= 1073741824.0)
+                size = String.Format("{0:##.##}", byteCount / 1073741824.0) + " GB";
+            else if (byteCount >= 1048576.0)
+                size = String.Format("{0:##.##}", byteCount / 1048576.0) + " MB";
+            else if (byteCount >= 1024.0)
+                size = String.Format("{0:##.##}", byteCount / 1024.0) + " KB";
+            else if (byteCount > 0 && byteCount < 1024.0)
+                size = byteCount.ToString() + " Bytes";
+
+            return size;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            calc.Stop();
+            btnAnalyzer.Enabled = true;
+            btnChoseFolder.Enabled = true;
+            btnStop.Enabled = false;
+        }
+    }
+
+
+    public class DirectorySizeCalculator
+    {
+        string _rootPath = "";
+        DirectoryInfo _rootFolder;
+        SortedDictionary<string, long> _sizes;
+        object _locker = new object();
+        Action<SortedDictionary<string, long>, bool> _onSizeUpdated;
+        bool _isOk = false;
+        public DirectorySizeCalculator(string rootPath, Action<SortedDictionary<string, long>, bool> onSizeUpdated = null)
+        {
+            _rootPath = rootPath;
+            _rootFolder = new DirectoryInfo(_rootPath);
+            _sizes = new SortedDictionary<string, long>();
+            _onSizeUpdated = onSizeUpdated;
+        }
+
+        List<Thread> _tasks = new List<Thread>();
+        public void Stop()
+        {
+            _tasks.ForEach(th => th.Abort());
+            _tasks.Clear();
+            _sizes.Clear();
+            _isOk = false;
+        }
+        public void Calculate()
+        {
+            _tasks.ForEach(th => th.Abort());
+            _tasks.Clear();
+            _sizes.Clear();
+            _isOk = false;
+            if (_onSizeUpdated != null)
+            {
+                var thrSizeUpdated = new Thread(() =>
+                  {
+                      Thread.Sleep(500);
+                      while (!_isOk)
+                      {
+                          _onSizeUpdated.Invoke(_sizes, _isOk);
+                          Thread.Sleep(500);
+                      }
+                      _onSizeUpdated.Invoke(_sizes, _isOk);
+                  });
+                thrSizeUpdated.Start();
+                _tasks.Add(thrSizeUpdated);
+            }
+            if (_rootFolder.Exists)
+            {
+                ConcurrentQueue<FileInfo> files = new ConcurrentQueue<FileInfo>();
+                var thrCrawlFiles = new Thread(() =>
+                {
+                    Thread.Sleep(100);
+                    CrawlFiles(files, _rootFolder, "*.*");
+                    _isOk = true;
+                });
+                thrCrawlFiles.Start();
+                _tasks.Add(thrCrawlFiles);
+                var thrAddFileSize = new Thread(() =>
+                {
+                    while (!_isOk)
+                    {
+                        FileInfo file;
+                        if (files == null || files.Count == 0)
+                        {
+                            Thread.Sleep(100);
+                        }
+                        else
+                        {
+                            if (files.TryDequeue(out file))
+                            {
+                                addFileSize(file, null);
+                            }
+                        }
+                    }
+                });
+                thrAddFileSize.Start();
+                _tasks.Add(thrAddFileSize);
+            }
+        }
+
+        IEnumerable<FileInfo> EnumerateFiles(DirectoryInfo path, string searchPattern)
+        {
+            return path.EnumerateFiles(searchPattern).Union(path.EnumerateDirectories().SelectMany(
+                d => { try { return EnumerateFiles(d, searchPattern); } catch { return new List<FileInfo>(); } }));
+        }
+
+        void CrawlFiles(ConcurrentQueue<FileInfo> files, DirectoryInfo path, string searchPattern)
+        {
+            foreach (var f in path.EnumerateFiles(searchPattern))
+            {
+                files.Enqueue(f);
+            }
+            foreach (var d in path.EnumerateDirectories())
+            {
+                try { CrawlFiles(files, d, searchPattern); }
+                catch
+                {
+
+                }
+            }
+        }
+
+        private void addFileSize(FileInfo file, DirectoryInfo directory)
+        {
+            var folder = directory == null ? file.Directory : directory;
+            if (folder != null)
+            {
+                if (!_sizes.ContainsKey(folder.FullName))
+                {
+                    lock (_locker)
+                    {
+                        _sizes.Add(folder.FullName, file.Length);
+                    }
+                }
+                else
+                {
+                    lock (_locker)
+                    {
+                        long size = _sizes[folder.FullName];
+                        _sizes[folder.FullName] = size + file.Length;
+                    }
+                }
+                if (!folder.FullName.Equals(_rootFolder.FullName))
+                {
+                    addFileSize(file, folder.Parent);
+                }
+            }
+        }
+    }
+}
